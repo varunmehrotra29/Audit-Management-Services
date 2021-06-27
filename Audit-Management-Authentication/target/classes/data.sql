efee0749ba3e6792dec5cd94789785a8d0103215
unknown <varunmehrotra2998@gmail.com>
2021-06-27 10:34:41 +0530

    first commit

---
+++ Audit-Management-Authentication/target/classes/data.sql
@@ -0,0 +1,4 @@
+insert into Project_Manager values ('varun','Varun@2998','Varun@2998');
+insert into Project_Manager values ('audit2','password2','password2');
+insert into Project_Manager values ('audit3','password3','password3');
+insert into Project_Manager values ('audit4','password4','password4');
